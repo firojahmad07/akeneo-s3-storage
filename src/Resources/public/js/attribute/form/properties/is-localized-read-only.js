c5d801623d78c8ed1be0f0140a8cf8cd88e9a836
Yury Turkulets <yuri.turkulec@ewave.com>
2020-11-19 08:28:21 +0300

    #397363 [385215][340035] [Project: TRU] Blank option displayed after switching on 'Enable localized read only' first time

--- src/Resources/public/js/attribute/form/properties/is-localized-read-only.js
+++ src/Resources/public/js/attribute/form/properties/is-localized-read-only.js
@@ -13,9 +13,7 @@ function (
         updateModel: function () {
             BaseField.prototype.updateModel.apply(this, arguments);
 
-            if (false === this.getFormData().is_localized_read_only) {
-                this.setData({read_only_locales: []}, {silent: true});
-            }
+            this.setData({read_only_locales: []}, {silent: true});
         }
     });
 });
